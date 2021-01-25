6ab2f092017ae64c900891b032a9d15b9d64c903
Marisa Vitale <marisa.vitale2306@gmail.com>
2021-01-25 19:40:50 +0100

    productlist component

---
+++ client/src/components/pages/ProductList/ProductList.js
@@ -0,0 +1,13 @@
+import './ProductList.css'
+import { Link } from "react-router-dom";
+import { Container } from 'react-bootstrap'
+
+const ProductList = () => {
+
+    return (
+        <h1>Our Product</h1>
+    )
+
+}
+
+export default ProductList
