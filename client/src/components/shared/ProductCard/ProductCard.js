1b25454e8e381bcb5ab125fa73a2df7c7f20f760
Marisa Vitale <marisa.vitale2306@gmail.com>
2021-01-26 23:03:13 +0100

    Rating component

--- client/src/components/shared/ProductCard/ProductCard.js
+++ client/src/components/shared/ProductCard/ProductCard.js
@@ -1,6 +1,7 @@
 import './ProductCard.css'
 import { Link } from 'react-router-dom'
 import { Col, Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const ProductCard = props => {
 
@@ -16,13 +17,7 @@ const ProductCard = props => {
                     <Card.Title className="mb-2"> {product.name}</Card.Title>
                     <Card.Text className="mb-2">
                         <p className="">{product.description.substring(0, 100)}...</p>
-                        <div class="rating">
-                            <span> <i class="fa fa-star"></i> </span>
-                            <span> <i class="fa fa-star"></i> </span>
-                            <span> <i class="fa fa-star"></i> </span>
-                            <span> <i class="fa fa-star"></i> </span>
-                            <span> <i class="fa fa-star"></i> </span>
-                        </div>
+                        <Rating></Rating>
                         <p className="">{product.price} €</p>
                     </Card.Text>
 
