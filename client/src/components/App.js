368a77513087919498f7679b6af336b87575c37d
Marisa Vitale <marisa.vitale2306@gmail.com>
2021-01-27 23:16:21 +0100

    Browser Router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,6 +6,7 @@ import Footer from './layout/Footer/Footer'
 
 function App() {
   return (
+
     <>
       <Navigation />
       <main>
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import reportWebVitals from './reportWebVitals';
 
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
